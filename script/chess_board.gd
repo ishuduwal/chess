916465faa49768614da895fc61cf4a6502d4faf2
ishuduwal <ishuduwal8@gmail.com>
2024-05-29 17:00:21 +0545

    remaining pieces making

--- script/chess_board.gd
+++ script/chess_board.gd
@@ -27,7 +27,12 @@ func create_sprite(area,piece):
 		"P":
 			full_name = "Pawn"
 			color = "White" if piece.get_child(2).name.substr(0,5) == "White" else "Black"
-			
+		"Z":
+			full_name = "Knight"
+			color = "White" if piece.get_child(2).name.substr(0,5) == "White" else "Black"
+		"K":
+			full_name = "King"
+			color = "White" if piece.get_child(2).name.substr(0,5) == "White" else "Black"
 	var new_sprite = Sprite2D.new()
 	print(color)
 	print(full_name)
@@ -49,30 +54,86 @@ func player_movement(area, piece):
 	print(global.selected_node.name)
 	match piece:
 		"P":
-			var area_name = str(area.name)  
-			var area_index = int(area_name)  
-			var new_area_index = area_index + 10  
-			var new_area_name = str(new_area_index)  
-			var new_area_node = get_node("../" + new_area_name)  
-			
-			if new_area_node:
-				var possible_move = Sprite2D.new() 
-				var new_image = preload("res://assets/gridsquare/predict.png")  
-				possible_move.texture = new_image  
-				possible_move.position = Vector2(0, 0)
-				possible_move.name = "possible_move"
-				new_area_node.add_child(possible_move) 
-				print("pawn moved to ", new_area_name)
-			else:
-				print("Failed to find node for new area: ", new_area_name)
-		"B":
-			print("bishop moved ", area.name)
-		"K":
-			print("king moved ", area.name)
-		"Q":
-			print("queen moved ", area.name)
-		"R":
-			print("rook moved ", area.name)
-		"Z":
-			print("Knight moved")
+			# Pawn movement logic
+			var area_name = str(area.name)
+			var area_index = int(area_name)
 
+			# Check if it's the first move of the pawn
+			var initial_row = 2
+			var is_first_move = (area_index >= initial_row * 10 and area_index < (initial_row + 1) * 10)
+
+			var new_area_indices = []
+
+			if is_first_move:
+				new_area_indices.append(area_index + 20)  # Two steps ahead
+
+			new_area_indices.append(area_index + 10)  # One step ahead
+
+			for new_area_index in new_area_indices:
+				var new_area_name = str(new_area_index)
+				var new_area_node = get_node("../" + new_area_name)
+
+				if new_area_node:
+					var possible_move = Sprite2D.new()
+					var new_image = preload("res://assets/gridsquare/predict.png")
+					possible_move.texture = new_image
+					possible_move.position = Vector2(0, 0)
+					possible_move.name = "possible_move"
+					new_area_node.add_child(possible_move)
+					print("Pawn moved to ", new_area_name)
+				else:
+					print("Failed to find node for new area: ", new_area_name)
+
+		"Z":
+			var area_name = str(area.name)
+			var area_index = int(area_name)
+			
+			var knight_moves = [
+				area_index - 12, area_index - 21,
+				area_index - 8, area_index - 19, 
+				area_index + 12, area_index + 21, 
+				area_index + 8, area_index + 19 
+			]
+
+			for new_area_index in knight_moves:
+				var new_area_name = str(new_area_index)
+				var new_area_node = get_node("../" + new_area_name)
+
+				if new_area_node:
+					var possible_move = Sprite2D.new()
+					var new_image = preload("res://assets/gridsquare/predict.png")
+					possible_move.texture = new_image
+					possible_move.position = Vector2(0, 0)
+					possible_move.name = "possible_move"
+					new_area_node.add_child(possible_move)
+					print("Knight moved to ", new_area_name)
+				else:
+					print("Failed to find node for new area: ", new_area_name)
+		"K":
+			var area_name = str(area.name)
+			var area_index = int(area_name)
+			var king_moves = [
+				area_index - 1,   # Move left
+				area_index + 1,   # Move right
+				area_index - 10,  # Move up
+				area_index + 10,  # Move down
+			]
+			for new_area_index in king_moves:
+				var new_area_name = str(new_area_index)
+				var new_area_node = get_node("../" + new_area_name)
+				if new_area_node:
+						var possible_move = Sprite2D.new()
+						var new_image = preload("res://assets/gridsquare/predict.png")
+						possible_move.texture = new_image
+						possible_move.position = Vector2(0, 0)
+						possible_move.name = "possible_move"
+						new_area_node.add_child(possible_move)
+						print("King moved to ", new_area_name)
+				else:
+					print("Failed to find node for new area: ", new_area_name)
+		"B":
+			print("Bishop moved ", area.name)
+		"Q":
+			print("Queen moved ", area.name)
+		"R":
+			print("Rook moved ", area.name)
