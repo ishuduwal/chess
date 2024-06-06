c628328164d4c722f1e00aa7d0a5d625312fc3b6
ishuduwal <ishuduwal8@gmail.com>
2024-06-06 13:17:45 +0545

    promoted pawn movement

--- script/Promotion.gd
+++ script/Promotion.gd
@@ -13,15 +13,17 @@ func _on_input_event(viewport, event, shape_idx):
 			match self.name.substr(0, 1):
 				"B":
 					new_texture_path = "res://assets/" + piece_color + "/Bishop.png"
+					
 				"Q":
 					new_texture_path = "res://assets/" + piece_color + "/Queen.png"
-				"K":
+				"Z":
 					new_texture_path = "res://assets/" + piece_color + "/Knight.png"
 				"R":
 					new_texture_path = "res://assets/" + piece_color + "/Rook.png"
 			
 			if new_texture_path != "":
 				sprite_pawn.texture = load(new_texture_path)
+				sprite_pawn.name = piece_color+self.name.substr(0, 1)+str(2)
 				print(piece_color.capitalize(), "pawn promoted to", self.name.substr(0, 1))
 
 			var promotion_pawn_node = get_parent() 
